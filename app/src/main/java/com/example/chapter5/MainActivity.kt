a370d2144f4e673bdf3d909c219480db6735b97f
suyeong2785 <qkdtpwls_@naver.com>
2024-01-10 01:36:53 +0900

    24/01/10 방수영 1. xml 데이터 객체 정의 및 rss데이터 retrofit2에 tickxml 컨버터 적용해서 가져오기 기능 구현

--- app/src/main/java/com/example/chapter5/MainActivity.kt
+++ app/src/main/java/com/example/chapter5/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.chapter5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.tickaroo.tikxml.TikXml
+import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
+
+    private var retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl("https://news.google.com/")
+        .addConverterFactory(
+            TikXmlConverterFactory.create(
+                TikXml.Builder()
+                    .exceptionOnUnreadXml(false)
+                    .build()
+            )
+        ).build()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newsService = retrofit.create(NewsService::class.java)
+        newsService.mainFeed().enqueue(object : Callback<NewsRss>{
+            override fun onResponse(call: Call<NewsRss>, response: Response<NewsRss>) {
+                Log.e("MainActivity", "${response.body()?.channel?.items}")
+            }
+
+            override fun onFailure(call: Call<NewsRss>, t: Throwable) {
+                t.printStackTrace()
+            }
+
+        })
+
     }
 }
---
+++ app/src/main/java/com/example/chapter5/NewsService.kt
@@ -0,0 +1,9 @@
+package com.example.chapter5
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface NewsService {
+    @GET("rss?hl=ko&gl=KR&ceid=KR:ko")
+    fun mainFeed(): Call<NewsRss>
+}
